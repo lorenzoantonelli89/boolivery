7903bbb40df1f73f631837a5e922cb9b7e48c918
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-06-16 10:18:29 +0200

    add foreign keys restaurant_id on order

--- boolivery/database/migrations/9999_06_15_125323_add_foreign_keys.php
+++ boolivery/database/migrations/9999_06_15_125323_add_foreign_keys.php
@@ -23,6 +23,11 @@ class AddForeignKeys extends Migration
                     ->references('id')
                     ->on('restaurants');
         });
+        Schema::table('orders', function (Blueprint $table) {
+            $table  ->foreign('restaurant_id', 'orderRestaurant')
+                    ->references('id')
+                    ->on('restaurants');
+        });
         Schema::table('category_restaurant', function (Blueprint $table) {
             $table  ->foreign('category_id', 'restaurantcategory')
                     ->references('id')
@@ -59,6 +64,9 @@ class AddForeignKeys extends Migration
         });
         Schema::table('plates', function (Blueprint $table){
             $table -> dropForeign('restaurantplate');
+        });
+        Schema::table('orders', function (Blueprint $table){
+            $table -> dropForeign('orderRestaurant');
         });
         Schema::table('category_restaurant', function (Blueprint $table){
             $table -> dropForeign('restaurantcategory');
