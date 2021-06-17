6e1d581c42f4531b8755624346a952f43952918d
Jacopo <jacopo.zando@gmail.com>
2021-06-17 15:23:58 +0200

    Identazione, homepage, foto

--- boolivery/resources/views/layouts/main-layout.blade.php
+++ boolivery/resources/views/layouts/main-layout.blade.php
@@ -5,16 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <!-- Google font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
     {{-- my script js --}}
     <script src="{{asset('/js/app.js')}}"></script>
-    {{-- my link css --}}
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+
 </head>
 <body>
-    <div id="app">
+    
+    <div id=app class=my-container>
         @include('components.header')
+
         @yield('content')
+
         @include('components.footer')
     </div>
+    
 </body>
 </html>
