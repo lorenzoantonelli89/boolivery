0f9fe9e22db9c66fa854cabebe214ba0283d3583
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-06-18 14:47:53 +0200

    update home, create route e page info-company

--- boolivery/app/Http/Controllers/GuestController.php
+++ boolivery/app/Http/Controllers/GuestController.php
@@ -8,10 +8,16 @@ use App\Restaurant;
 
 class GuestController extends Controller
 {
+    // funzione che ritora in pagina il ristorante nel dettaglio 
     public function detailsRestaurant($id){
 
         $restaurant = Restaurant::findOrFail($id);
 
         return view('pages.restaurant-details', compact('restaurant'));
+    }
+    // funzione che riporta alla pagina chi siamo
+    public function infoCompany(){
+
+        return view('pages.info-company');
     }
 }
