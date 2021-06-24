f5d1e5235401e7930d07e1403671fcd9ac174e10
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-06-24 18:30:37 +0200

    update homepage

--- boolivery/resources/views/pages/home.blade.php
+++ boolivery/resources/views/pages/home.blade.php
@@ -11,27 +11,23 @@
         <div class=myjumbotron>
           <div class="div-margin">
             <div class="welcome ">
-              <!-- <div class="logo-login">
-                <img class="logo" src={{ asset('/storage/graphics/logocompleto.png') }} alt="">
-                <a class="blink" href="#">LOGIN</a>
-              </div> -->
-                  <div class="title-research">                  
-                    <h1>I piatti che ami, a domicilio</h1>
-                    <div class="research-request">
-                      <span>Cerca il tuo ristorante preferito</span> 
-                        <div class="input-adresse">
-                          <input id="guest-request" type="text" name="" value="" placeholder="Cerca il tuo ristorante preferito..." v-model="searchRestaurant" v-on:keyup="filteredRestaurantsName">
-                        </div>
+              <div class="title-research">                  
+                <h1>I piatti che ami, a domicilio</h1>
+                <div class="research-request" v-if="categoryChecked == 0">
+                  <span>Cerca il tuo ristorante preferito</span> 
+                    <div class="input-adresse" >
+                      <input id="guest-request" type="text" name="" value="" placeholder="Cerca il tuo ristorante preferito..." v-model="searchRestaurant" v-on:keyup="filteredRestaurantsName">
                     </div>
-                  </div>   
-                  <div class="categories">
-                      <ul id="category-container-list">
-                          <li class="category-list" v-for="category in categories">
-                              <label for="">@{{category.name}}</label>
-                              <input type="checkbox" :value="category.id"  v-model="categoryChecked" v-on:change="filteredRestaurantsCategory">
-                          </li>
-                      </ul>
-                  </div>     
+                </div>
+                </div>   
+                <div class="categories">
+                    <ul id="category-container-list" v-if="searchRestaurant == ''">
+                        <li class="category-list" v-for="category in categories">
+                            <label for="">@{{category.name}}</label>
+                            <input type="checkbox" :value="category.id"  v-model="categoryChecked" v-on:change="filteredRestaurantsCategory">
+                        </li>
+                    </ul>
+                </div>     
               </div>
           </div>          
         </div>
@@ -55,89 +51,53 @@
             </div>
           </div>
         </div>
-        {{-- <div class="main-sec-1" v-else-if="searchRestaurant != ''">
+
+        <!-- SECONDA SEZIONE, VISIONE DEI PIATTI -->
+        <div id="section-2">
+          <div id="top-rating-plates">
+            <h1><i><sup><i class="fas fa-star"></i></sup>Top ratings</i></h1>
+          </div>
+          <div class="flexible-carusel">
+            <div id="prev" class="chevron">
+              <i class="fas fa-chevron-left" v-on:click='prevImg'></i>
+            </div>
+            <!--<div class="position-carousel"></div>-->
+            <div class="div-margin">
+              <div id="plates-info">
+                {{-- <img :src="'/storage/restaurant-plates/' + platesPopular[counter].image" alt="Immagine di portate">
+                  <div id="text-plates">
+                    <h3><i>@{{platesPopular[counter].name}}</i></h3>
+                    <p><i>@{{platesPopular[counter].description}}</i></p>
+                  </div> --}}
+              </div>
+            </div>
+            <div id="next" class="chevron">
+              <i class="fas fa-chevron-right" v-on:click='nextImg'></i>          
+            </div>            
+          </div>
+        </div>
+
+        <!-- TERZA SEZIONE, SEZIONE PER I RISTORATORI -->
+        <div class="main-sec-3">
           <div class="div-margin">
-            <div class="main-1-container">
-              <ul>
-                <li v-for="elem in filteredRestaurantsName" v-on:click="getActiveRestaurant(elem)">
-                  <a :href="getHref">
-                    <div class="restaurants">
-                      <img :src="'/storage/restaurant-profile/' + elem.image_profile " alt="Copertina ristorante">
-                      <div id="text">
-                        <h3 >@{{ elem.name }}</h3>
-                      </div>
-                    </div>
-                  </a>        
-                </li>
-              </ul>
+            <div class="business-adv">
+              <img class="img-icon" src={{ asset('/storage/graphics/restaurant_icon.png') }} alt="">
+              <h1>Diventa partner Boolivery</h1>
+              <span>Aumenta le tue vendite fino al 30% grazie alle consegne a domicilio</span>
+              <a class="blink" href="#">LOGIN</a>     
             </div>
           </div>
         </div>
-        <div class="main-sec-1" v-else>
-          <div class="div-margin">
-            <div class="main-1-container">
-              <ul>
-                <li v-for="elem in filteredRestaurantsCategory" v-on:click="(getActiveRestaurant(elem), getActivePlates)">
-                  <a :href="getHref">
-                    <div class="restaurants">
-                      <img :src="'/storage/restaurant-profile/' + elem.image_profile " alt="Copertina ristorante">
-                      <div id="text">
-                        <h3 >@{{ elem.name }}</h3>
-                      </div>
-                    </div>
-                  </a>        
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div> --}}
 
-          <!-- SECONDA SEZIONE, VISIONE DEI PIATTI -->
-          <div id="section-2">
-            <div id="top-rating-plates">
-              <h1><i><sup><i class="fas fa-star"></i></sup>Top ratings</i></h1>
-            </div>
-            <div class="flexible-carusel">
-              <div id="prev" class="chevron">
-                <i class="fas fa-chevron-left" v-on:click='prevImg'></i>
-              </div>
-              <!--<div class="position-carousel"></div>-->
-              <div class="div-margin">
-                <div id="plates-info">
-                  {{-- <img :src="'/storage/restaurant-plates/' + platesPopular[counter].image" alt="Immagine di portate">
-                    <div id="text-plates">
-                      <h3><i>@{{platesPopular[counter].name}}</i></h3>
-                      <p><i>@{{platesPopular[counter].description}}</i></p>
-                    </div> --}}
-                </div>
-              </div>
-              <div id="next" class="chevron">
-                <i class="fas fa-chevron-right" v-on:click='nextImg'></i>          
-              </div>            
-            </div>
+        <!-- CONTENITORE CONTATTI IN ABSOLUTE -->
+        <div id="contacts">
+          <div id="mail">
+            <i class="fas fa-at"></i>
           </div>
-
-          <!-- TERZA SEZIONE, SEZIONE PER I RISTORATORI -->
-          <div class="main-sec-3">
-            <div class="div-margin">
-              <div class="business-adv">
-                <img class="img-icon" src={{ asset('/storage/graphics/restaurant_icon.png') }} alt="">
-                <h1>Diventa partner Boolivery</h1>
-                <span>Aumenta le tue vendite fino al 30% grazie alle consegne a domicilio</span>
-                <a class="blink" href="#">LOGIN</a>     
-              </div>
-            </div>
+          <div id="phone">
+            <i class="fas fa-phone"></i>
           </div>
-
-          <!-- CONTENITORE CONTATTI IN ABSOLUTE -->
-          <div id="contacts">
-            <div id="mail">
-              <i class="fas fa-at"></i>
-            </div>
-            <div id="phone">
-              <i class="fas fa-phone"></i>
-            </div>
-          </div>
+        </div>
       </div>
     </main>
 
