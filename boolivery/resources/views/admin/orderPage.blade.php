9366bba092fab99d5c49906f5c7e4089a58da3ab
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-06-29 16:16:05 +0200

    sistemata grafica order details, list e stats

--- boolivery/resources/views/admin/orderPage.blade.php
+++ boolivery/resources/views/admin/orderPage.blade.php
@@ -1,58 +1,90 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <main>
-        <div class="container" style="width:1140px; margin: auto">
-            {{-- titolo --}}
-            <h1>Ordine #{{$order->id}}</h1>
-            <div>
-                <span>Ordine effettuato da:</span>
-                <span><i>{{$order->name}} {{$order->lastname}}</i></span>
+    <main id="order-detail">
+        <div class="container">
+            <div class="order-block">
+                <div class="order-details">
+                    {{-- titolo --}}
+                    <h1>Ordine #{{$order->id}}</h1>
+                    <div>
+                        <span>Ordine effettuato da:</span>
+                        <span><i><strong>{{$order->name}} {{$order->lastname}}</strong></i></span>
+                    </div>
+                    <div>
+                        <span>Email cliente:</span>
+                        <span><i><strong>{{$order->customer_email}}</strong></i></span>
+                    </div>
+                    <div>
+                        <span>Indirizzo di consegna:</span>
+                        <span><i><strong>{{$order->shipping_address}}</strong></i></span>
+                    </div>
+                    <div>
+                        <span>Ordine da consegnare in data:</span>
+                        <span><i><strong>{{$order->date_delivery}}</strong></i></span>
+                        <span> alle ore </span>
+                        <span><i><strong>{{$order->time_delivery}}</strong></i></span>
+                    </div>
+                    {{-- totale in € --}}
+                    <div class="recap">
+                        <span>Totale ordine:</span>
+                        <span><i><strong>{{$order->total_price}}€ </strong></i></span>
+                        @php
+                            $sum = 0;
+                            foreach($order->plates as $plate){
+                                $sum += $plate->price;
+                            }
+                        @endphp
+                        @if ($sum < 20)
+                        <span>di cui<i><strong> 5€</strong></i> di consegna</span>  
+                        @else
+                            <span>con <i><strong>consegna gratuita</strong></i></span> 
+                        @endif
+                    </div>
+                </div>
+                {{-- link al totale ordini --}}
+                <div>
+                    <div class="back-button">
+                        <a href="{{route('showOrders', encrypt($restaurant -> id))}}">TORNA AGLI ORDINI</a>
+                    </div>
+                </div>
             </div>
-            <div>
-                <span>Email cliente:</span>
-                <span><i>{{$order->customer_email}}</i></span>
-            </div>
-            <div>
-                <span>Indirizzo di consegna:</span>
-                <span><i>{{$order->shipping_address}}</i></span>
-            </div>
-            <div>
-                <span>Ordine da consegnare in data:</span>
-                <span><i>{{$order->date_delivery}}</i></span>
-                <span> alle ore </span>
-                <span><i>{{$order->time_delivery}}</i></span>
-            </div>
+            {{-- lista piatti ordinati --}}
             <div>
                 <h2>Piatti ordinati</h2>
                 <ul>
-                    @foreach ($order->plates as $plate)
+                    {{-- definisco array di ID piatti ordinati --}}
+                    @php
+                        $plateIds = [];
+                        foreach($order->plates as $plate){
+                            $plateIds[] = $plate->id;
+                        }
+                    @endphp
+                    {{-- stampo tutti i piatti del menu, filtrando quelli ordinati con b-if --}}
+                    @foreach ($restaurant->plates as $plate)
+                        @if (in_array($plate->id, $plateIds))
                         <li>
                             <div>{{$plate->name}}</div>
-                            <div>{{$plate->price}}</div>
-                            <img src="{{ asset('/storage/restaurant-plates')}}/{{ $plate -> image }}" alt="" width="50px">
-                        </li> 
+                            <div>{{$plate->price}}€</div>
+                            <div>
+                                {{-- qty ordinata --}}
+                                @php
+                                    $counter = 0;
+                                    foreach ($plateIds as $OrderedItem) {
+                                        if($OrderedItem == $plate->id){
+                                            $counter++;
+                                        }
+                                    }
+                                @endphp
+                                Qty: {{$counter}}
+                            </div>
+                            <img src="{{ asset('/storage/restaurant-plates')}}/{{ $plate -> image }}" alt="">
+                        </li>   
+                        @endif
                     @endforeach
                 </ul>
             </div>
-            <div>
-                <span>Totale ordine:</span>
-                <span><i>{{$order->total_price}}</i></span>
-                @php
-                    $sum = 0;
-                    foreach($order->plates as $plate){
-                        $sum += $plate->price;
-                    }
-                @endphp
-                @if ($sum < 20)
-                  <div>Di cui 5€ di consegna</div>  
-                @else
-                    <div>Con consegna gratuita</div> 
-                @endif
-            </div>
-
-
-        
+        </div>
     </main>
 @endsection
 
